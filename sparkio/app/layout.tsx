b472606dc7b222ede654f198de003930b082e88d
Your Name <info.rishankp@gmail.com>
2025-12-25 09:53:20 +0530

    feat: add Vercel Analytics

--- sparkio/app/layout.tsx
+++ sparkio/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -60,8 +61,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </SessionProvider>
           </QueryProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
-    </html>
+    </html >
   );
 }
 
